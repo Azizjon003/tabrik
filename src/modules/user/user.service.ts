3ef1bae0e7f1e2fbe50e40fed766e7642c581c46
azizjon003 <aliqulovazizjon68@gmail.com>
2024-09-06 10:54:12 +0500

    Fixed

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from 'src/lib/prisma/prisma.service';
+
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    const users = await this.prisma.user.findMany();
+    return {
+      message: 'All users',
+      data: users,
+    };
+  }
+}
